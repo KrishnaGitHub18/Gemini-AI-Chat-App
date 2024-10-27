61647d5c344fa450e073903a950a93af052200d2
KrishnaGitHub18 <krishnadeheriya1803@gmail.com>
2024-10-27 15:03:28 +0530

    Gemini API key configured

--- components/MessageBox.jsx
+++ components/MessageBox.jsx
@@ -1,25 +1,48 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button, TextInput, } from 'react-native'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 export default function MessageBox() {
 
+  const [text, setText] = useState('');
+
   const handleGeminiAPI = async () => {
+
+    // const que = text;
+    // console.log(que);
+    // return; 
+
     try {
-        const que = "hey";
+      const que = text;
+
+      if (text) {
         const response_API = await axios.post('https://gemini-ai-chat-app.vercel.app/api/apiHandler', { que });
-        console.log(response_API.data);
+        const cleanedText = response_API.data.replace(/\*/g, '').replace(/\n+/g, ' ');
+        console.log(cleanedText);
+
+      } else {
+        console.log('Please enter a question');
+      }
+
+      setText('');
     } catch (error) {
-        console.log(error, "error in API handling")
+      console.log(error, "error in API handling")
     }
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.textHead_css}>MessageBox</Text>
+      {/* <Text style={styles.textHead_css}>MessageBox</Text> */}
+
+      <TextInput
+        style={styles.textInput}
+        onChangeText={setText}
+        value={text}
+        placeholder={'Please type here…'}
+      />
+
       <View style={styles.send_button} onTouch={handleGeminiAPI}>
-        {/* <Text style={styles.textSend_css}>Send</Text> */}
-        <Button 
+        <Button
           title='Send'
           onPress={handleGeminiAPI}
           style={styles.textSend_css}
@@ -53,5 +76,8 @@ const styles = StyleSheet.create({
   textSend_css: {
     color: "black",
     fontWeight: '500'
+  },
+  textInput: {
+    color: 'black'
   }
 })
